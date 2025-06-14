039c5c26f9dae3e1a79d633d85cd1790e1b81b3c
Denisse <88408416+dennicg222@users.noreply.github.com>
2025-06-13 22:17:31 -0500

    Established Database and UserAuthentication

--- Backend/LunaVerseBackEnd/src/main/java/org/lunaverse/controller/AuthenticationController.java
+++ Backend/LunaVerseBackEnd/src/main/java/org/lunaverse/controller/AuthenticationController.java
@@ -1,8 +1,47 @@
 package org.lunaverse.controller;
 
+import org.lunaverse.security.jwt.TokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 
 
 
+
+@RestController
 public class AuthenticationController {
+
+    private final TokenProvider tokenProvider;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationController(TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+        this.tokenProvider = tokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public ResponseEntity<TokenDto> authorize(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManager.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        boolean rememberMe = loginDto.isRememberMe() != null && loginDto.isRememberMe();
+        String jwt = tokenProvider.createToken(authentication, rememberMe);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt);
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    }
+
 }
--- Backend/database/schema.sql
+++ Backend/database/schema.sql
@@ -2,59 +2,82 @@ BEGIN TRANSACTION;
 
 CREATE TABLE users(
 user_id SERIAL,
-email varchar(50) NOT NULL,
+email varchar(50) NOT NULL UNIQUE,
 password_hash varchar(200) NOT NULL,
-created_at time, 
-updated_at time,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE PROFILE(
-user_id SERIAL,
+CREATE TABLE profile(
+profile_id SERIAL,
+user_id INT NOT NULL,
 display_name varchar(50) NOT NULL UNIQUE,
 bio varchar(200),
 profile_pic_url varchar(200),
 background_music_url varchar(200),
-custom_css varchar(200),
-created_at DEFAULT TIME,
-updated_at DEFAULT TIME,
-CONSTRAINT PK_user PRIMARY KEY (user_id)
+custom_theme TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT PK_profiles PRIMARY KEY (profile_id),
+CONSTRAINT FK_profile_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE POSTS(
-user_id SERIAL, 
-content,
+CREATE TABLE posts(
+post_id SERIAL, 
+user_id INTEGER NOT NULL,
+content TEXT NOT NULL,
 media_url varchar(200),
-created_at DEFAULT TIME,
-updated_at DEFAULT TIME,
-CONSTRAINT PK_user PRIMARY KEY (user_id)
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT PK_posts PRIMARY KEY (post_id),
+CONSTRAINT FK_posts_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE FRIENDS(
-user_id SERIAL, 
-requester_id SERIAL,
-receiver_id SERIAL,
+CREATE TABLE friends(
+friend_id SERIAL, 
+requester_id INTEGER NOT NULL,
+receiver_id INTEGER NOT NULL,
 status varchar(50),
-request_at DEFAULT TIME,
-responded_at DEFAULT TIME,
-CONSTRAINT PK_user PRIMARY KEY(user_id)
+request_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+responded_at TIMESTAMP,
+CONSTRAINT PK_friends PRIMARY KEY(friend_id),
+CONSTRAINT FK_requester FOREIGN KEY (requester_id) REFERENCES users(user_id) ON DELETE CASCADE,
+CONSTRAINT FK_receiver FOREIGN KEY (receiver_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE COMMENTS(
-id SERIAL,
-post_id SERIAL,
-user_id SERIAL,
-content varchar(75),
-created_at DEFAULT TIME,
-CONSTRAINT PK_post PRIMARY KEY(id)
+CREATE TABLE comments(
+comment_id SERIAL,
+post_id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+content varchar(300) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT PK_comments PRIMARY KEY(comment_id),
+CONSTRAINT FK_comment_post FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
+CONSTRAINT FK_comment_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE LIKES(
-id SERIAL,
-post_id SERIAL,
-user_id SERIAL,
-created_at DEFAULT TIME,
-CONSTRAINT PK_post PRIMARY KEY(id)
+CREATE TABLE likes(
+like_id SERIAL,
+post_id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT PK_likes PRIMARY KEY(like_id),
+CONSTRAINT fk_like_post FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE,
+CONSTRAINT fk_like_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE messages(
+message_id SERIAL,
+sender_id INTEGER NOT NULL,
+receiver_id INTEGER NOT NULL,
+subject VARCHAR(100),
+body TEXT NOT NULL,
+is_read BOOLEAN DEFAULT FALSE,
+sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT pk_messages PRIMARY KEY (message_id),
+CONSTRAINT fk_messages_sender FOREIGN KEY (sender_id) REFERENCES users(user_id),
+CONSTRAINT fk_messages_receiver FOREIGN KEY (receiver_id) REFERENCES users(user_id)
 );
 
 COMMIT TRANSACTION;
